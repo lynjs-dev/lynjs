901834cae12d224d6fd73e535882b95a3fdd57c6
MoGi <mogiblahblah@gmail.com>
2025-09-17 16:22:29 +0900

    chore: fixed docs workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.DOCS_BASE || '/',
   integrations: [
     sitemap(),
     starlight({
