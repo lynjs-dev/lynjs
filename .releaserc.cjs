7a8b3feeee17a1ecdeb2273ac99366401326ea51
MoGi <mogiblahblah@gmail.com>
2025-10-14 14:45:53 +0900

    fix: fix `.releaserc.cjs` - update commit files

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -54,13 +54,7 @@ module.exports = {
     isCI && [
       '@semantic-release/git',
       {
-        assets: [
-          'CHANGELOG.md',
-          'package.json',
-          'package-lock.json',
-          'packages/babel-preset-lynjs/package.json',
-          'packages/babel-preset-lynjs/package-lock.json',
-        ],
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', 'packages/babel-preset-lynjs/package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
